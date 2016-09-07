de39e74757b2cb60c5794d31c9fd0109c67f61b4
Kamil Pyc <kamil.pyc@allegrogroup.com>
2016-09-07 09:09:07 +0200

    initial commit

---
+++ FastlaneTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastlaneTest
+//
+//  Created by Kamil Pyc on 07/09/16.
+//  Copyright © 2016 KamilPyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
